58f3408fad271e1f543767b88d5d9d46c2bee724
gaurav-8877 <gv88877@gmail.com>
2026-08-03 19:05:23 +0530

    comit

---
+++ KthLargest.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class KthLargest {
+public:
+    int k;
+    priority_queue<int,vector<int>,greater<int>>pq;
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        int n = nums.size();
+        for(int i = 0;i<n;i++){
+            pq.push(nums[i]);
+            if(pq.size()>k)pq.pop();
+        }
+    }
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>k)pq.pop();
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
---
+++ lastStoneWeight.cpp
@@ -0,0 +1,21 @@
+// Last Stone Weight
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(auto it : stones){
+            pq.push(it);
+        }
+        while(pq.size()>1){
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            if(x!=y)pq.push(x-y);
+        }
+        if(pq.size()==1)return pq.top();
+        else return 0;
+    }
+};
